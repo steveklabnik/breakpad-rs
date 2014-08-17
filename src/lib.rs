106a56ceef5b589545932cb361337497767a3e16
Corey Richardson <corey@octayn.net>
2014-08-17 00:47:01 -0400

    First pass at windows support

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,7 @@
 extern crate libc;
 
 #[cfg(target_os = "macos")]
-compile_fatal!("breakpad not yet supported on OS X")
-
-#[cfg(windows)]
-compile_fatal!("breakpad not yet support on Windows")
+compile_error!("breakpad not yet supported on OS X")
 
 pub struct ExceptionHandler {
     eh: *mut libc::c_void
@@ -28,7 +25,26 @@ impl ExceptionHandler {
             eh: eh
         }
     } }
+}
 
+#[cfg(target_os = "windows")]
+impl ExceptionHandler {
+    /// Create a new ExceptionHandler that will write crash dumps into `path`
+    pub fn new(path: &Path) -> ExceptionHandler { unsafe {
+        use std::ptr::{null, mut_null};
+        use std::mem::transmute;
+        let mut s = path.display().to_string().to_utf16();
+        s.push(0); // NUL terminate
+        let eh = ffi::rust_breakpad_exceptionhandler_new(s, transmute(null::<()>()),
+                                                         transmute(null::<()>()), mut_null(), 1);
+        ExceptionHandler {
+            eh: eh
+        }
+    } }
+
+}
+
+impl ExceptionHandler {
     /// Force writing a crash dump.
     ///
     /// Should *not* be called after a crash, as this uses the heap.
@@ -66,6 +82,30 @@ pub mod ffi {
         pub fn rust_breakpad_descriptor_free(desc: *mut libc::c_void);
 
         pub fn rust_breakpad_exceptionhandler_new(desc: *mut libc::c_void,
+                                              fcb: FilterCallback,
+                                              mcb: MinidumpCallback,
+                                              context: *mut libc::c_void,
+                                              install_handler: libc::c_int) -> *mut libc::c_void;
+        pub fn rust_breakpad_exceptionhandler_write_minidump(eh: *mut libc::c_void) -> libc::c_int;
+        pub fn rust_breakpad_exceptionhandler_free(eh: *mut libc::c_void);
+    }
+}
+
+#[cfg(target_os = "windows")]
+pub mod ffi {
+    use libc;
+
+    pub type FilterCallback = extern fn(context: *mut libc::c_void) -> libc::c_int;
+    pub type MinidumpCallback = extern fn(desc: *mut libc::c_void,
+                                          context: *mut libc::c_void,
+                                          succeeded: libc::c_int) -> libc::c_int;
+
+    #[link(name = "stdc++")]
+    extern { }
+
+    #[link(name = "breakpad-dll")]
+    extern {
+        pub fn rust_breakpad_exceptionhandler_new(desc: *const libc::char,
                                               fcb: FilterCallback,
                                               mcb: MinidumpCallback,
                                               context: *mut libc::c_void,
